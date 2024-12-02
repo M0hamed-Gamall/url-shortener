fa352559e0871ab7a7380a07a343247cb6b38202
M0hamed-Gamall <jimmy.mans00ur@gmail.com>
2024-12-02 03:03:29 +0200

    middleware to handle post request added & insert into database

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,24 @@
 const router = require('express').Router();
+const Url = require('../models/urlschema')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'Express' });
 });
+
+router.post('/' , async (req , res , next)=>{
+    try{
+        const newUrl = new Url({
+            original : req.body.urlInput,
+            alias    : req.body.aliasInput
+        })
+
+        await newUrl.save();
+        console.log("new url added successfully");
+        res.redirect('/');
+    }catch(err){
+        console.error("can't add new url :" , err);
+    }
+})
 
 module.exports = router;
